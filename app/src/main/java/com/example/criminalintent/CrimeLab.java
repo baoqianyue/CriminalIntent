e5882d223b2d50b3a935689b807ee31d1ef44e37
stbtkl <987132349@qq.com>
2017-04-10 20:53:59 +0800

    sql

--- app/src/main/java/com/example/criminalintent/CrimeLab.java
+++ app/src/main/java/com/example/criminalintent/CrimeLab.java
@@ -1,6 +1,12 @@
 package com.example.criminalintent;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.criminalintent.Database.CrimeBaseHelper;
+import com.example.criminalintent.Database.CrimeDbSchema.CrimeTable;
+import com.example.criminalintent.Database.CrimeDbSchema.CrimeTable.Cols;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,7 +18,8 @@ import java.util.UUID;
 
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
-    private List<Crime> mCrimes;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
 
     public static CrimeLab get(Context context) {
         if (sCrimeLab == null) {
@@ -22,23 +29,40 @@ public class CrimeLab {
     }
 
     private CrimeLab(Context context) {
-        mCrimes = new ArrayList<Crime>();
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext)
+                .getWritableDatabase();
     }
 
     public void addCrime(Crime c) {
-        mCrimes.add(c);
+        ContentValues values = getContentValues(c);
+        mDatabase.insert(CrimeTable.NAME, null, values);
     }
 
     public List<Crime> getCrimes() {
-        return mCrimes;
+        return new ArrayList<>();
     }
 
     public Crime getCrime(UUID id) {
-        for (Crime crime : mCrimes) {
-            if (crime.getId().equals(id)) {
-                return crime;
-            }
-        }
         return null;
+    }
+
+    public void updateCrime(Crime crime) {
+        String uuidString = crime.getId().toString();
+        ContentValues values = getContentValues(crime);
+
+        mDatabase.update(CrimeTable.NAME, values,
+                Cols.UUID + " = ?",
+                new String[]{uuidString});
+    }
+
+    private static ContentValues getContentValues(Crime crime) {
+        ContentValues values = new ContentValues();
+        values.put(Cols.UUID, crime.getId().toString());
+        values.put(Cols.TITLE, crime.getTitle());
+        values.put(Cols.DATE, crime.getDate().getTime());
+        values.put(Cols.SOLVED, crime.isSolved() ? 1 : 0);
+
+        return values;
     }
 }
---
+++ app/src/main/java/com/example/criminalintent/Database/CrimeBaseHelper.java
@@ -0,0 +1,47 @@
+package com.example.criminalintent.Database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.criminalintent.Database.CrimeDbSchema.CrimeTable;
+
+/**
+ * Created by 鲍骞月 on 2017/4/9.
+ */
+
+/**
+ * 这个类主要完成一下任务
+ * 1.确认目标数据库是否存在
+ * 2.如果不存在，首先创建数据库，然后进行数据库初始化
+ * 3.如果存在，检查数据库是否为最新版本
+ * 4.如果是旧版本，就更新
+ */
+
+public class CrimeBaseHelper extends SQLiteOpenHelper {
+    private static final int VERSION = 1;
+    private static final String DATABASE_NAME = "crimeBase.db";
+
+    public CrimeBaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //创建初始代码
+        db.execSQL("create table " + CrimeTable.NAME + "(" +
+                " _id integer primary key autoincrement, " +
+                CrimeTable.Cols.UUID + ", " +
+                CrimeTable.Cols.TITLE + ", " +
+                CrimeTable.Cols.DATE + ", " +
+                CrimeTable.Cols.SOLVED +
+                ")"
+        );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
